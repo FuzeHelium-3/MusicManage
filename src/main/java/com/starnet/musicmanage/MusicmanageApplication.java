b49b7ca877b50b3a4350f2a4bea80e20d38ba037
hxy <iamhxy@qq.com>
2020-08-04 16:40:05 +0800

    test test branch merge on branch test

--- src/main/java/com/starnet/musicmanage/MusicmanageApplication.java
+++ src/main/java/com/starnet/musicmanage/MusicmanageApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MusicmanageApplication {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test for branch test");
     }
 
 }
